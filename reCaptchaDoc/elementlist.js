235a1b879cc51f0bf2128462705f57e3ecbf7b85
cichy <d3ut3r@gmail.com>
2016-03-24 19:27:18 +0100

    reCaptcha Docs

---
+++ reCaptchaDoc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","DevStrefa\\ReCaptcha\\ReCaptcha"],["c","DevStrefa\\ReCaptcha\\Response"],["c","DevStrefa\\ReCaptcha\\Senders\\FgcSender"],["c","DevStrefa\\ReCaptcha\\Senders\\SenderInterface"]];
